46ef81228eaaaf61e633a7de6db9a1e3f6193b9b
serg <sergeysharay1987@gmail.com>
2021-12-10 21:03:19 +0300

    New files were added.

--- calculated_distance/blueprint.py
+++ calculated_distance/blueprint.py
@@ -1,58 +1,12 @@
-import os
-
-import shapely.geometry
-from flask import Blueprint, render_template, make_response, url_for
-from werkzeug.utils import redirect
+from flask import Blueprint, render_template
 from calculated_distance.forms import CalculateDistanceForm
 from flask import request, flash
 from loguru import logger
-import requests
+#import requests
 from calculated_distance.logic import *
 
-
-
-
-
-API_KEY = 'cbddbd2c-95ce-4aa1-ba5a-5d0416597c20'
 calculated_distance = Blueprint('calculated_distance', __name__, template_folder='templates')
-dir = os.getcwd()
-def make_url(address):
-    return f'https://geocode-maps.yandex.ru/1.x/?apikey={API_KEY}&geocode={address}&format=json'
 
-
-def get_data_from_response(url):
-    req = requests.get(url)
-    data = req.json()
-    return data
-
-
-# logging.basicConfig(level=20, filename='my_log.log')
-
-
-# @calculated_distance.route('/', methods=['POST', 'GET'])
-# def index():
-#     form = CalculateDistanceForm()
-#     if request.method == 'POST':
-#
-#         address = request.form['address']
-#         bound_form = CalculateDistanceForm(address=address)
-#         # print(f'bound_form["address"]: {bound_form.address.data}')
-#         if bound_form.validate():
-#             url = make_url(address)
-#             data = get_data_from_response(url)
-#             print(type(data))
-#             # print(response.is_json)
-#             logger.add('info.log', format='{time} {message}', level='INFO')
-#             logger.info(f'{address} {data}')
-#             print('Helloo')
-#             # response = make_response()
-#             return redirect(make_url(address))
-#         else:
-#             form = CalculateDistanceForm(address=address)
-#             print(form.address.errors)
-#
-#             return render_template('calculated_distance/index.html', form=form)
-#     return render_template('calculated_distance/index.html', form=form)
 
 @calculated_distance.route('/', methods=['POST', 'GET'])
 def index():
@@ -65,12 +19,12 @@ def index():
     if request.method == 'POST':
         address = request.form['address']
         bound_form = CalculateDistanceForm(data={'address': address})
+        print(f'os.getcwd(): {os.getcwd()}')
         if bound_form.validate():
             coords_of_address = geocode_address(bound_form.data['address'])
             distance = find_distance(coords_of_address)
             bound_form = CalculateDistanceForm(data={'address': address, 'distance': distance})
-            logger.add('info.log', format='{time} {message}', level='INFO')
-            logger.info(f'Растояние от МКАД до {address} равно {distance}')
+            write_in_log(address, distance)
             return render_template('calculated_distance/index.html', form=bound_form)
         elif not bound_form.validate():
             print(f'bound_form.errors still have error {bound_form.errors}')
--- calculated_distance/forms.py
+++ calculated_distance/forms.py
@@ -1,15 +1,27 @@
 from wtforms import Form, TextField, TextAreaField
-from wtforms.validators import DataRequired, InputRequired, ValidationError
+from wtforms.validators import DataRequired, ValidationError
+from wtforms.widgets import TextInput
+
+from .logic import geocode_address
 
 
 class CalculateDistanceForm(Form):
     """Create form for """
-    address = TextAreaField('Address', validators=[
-        DataRequired('Please enter the address'),
-    ])
+    address: str = TextAreaField('Address', validators=[
+        DataRequired('Please enter the address')])
     distance = TextField('Distance', render_kw={'readonly': True})
 
-    # def validate_address(self):
-    #     # address = '0'
-    #     if self.address == '0':
-    #         raise ValidationError('address should not be the 0')
+    def validate_address(form, field):
+        address: str = field.data
+        if len(address.split()) == 1 and 'Russia' in address:
+            raise ValidationError('Add name of locality (city, town etc.)')
+        # if len(field.data) <= 1:
+        #     raise ValidationError('Name must be less than 50 characters')
+        if address.isdigit():
+            raise ValidationError('Incorrect data')
+        try:
+            geocode_address(address)
+            print(address)
+        except (AttributeError, TypeError):
+            raise ValidationError(
+                'The "address" field must contain at least name of the country or name of the locality or both.')
