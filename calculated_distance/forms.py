6c32380df96af2638084a38fa7416152f1a0fdb4
serg <sergeysharay1987@gmail.com>
2021-12-14 14:03:40 +0300

    New field was added informs.py

--- calculated_distance/forms.py
+++ calculated_distance/forms.py
@@ -1,18 +1,35 @@
+from geopy import Location
 from wtforms import Form, TextField, TextAreaField
 from wtforms.validators import DataRequired, ValidationError
 from wtforms.widgets import TextInput
+from .logic import geocode_address, ya_geocoder, get_toponims, make_dict
 
-from .logic import geocode_address
+set_nessesary = {'country', 'locality', 'street'}
+nessesary_info_1 = {'country', 'locality', 'house'}
+nessesary_info_2 = {'country', 'locality', 'metro'}
 
 
 class CalculateDistanceForm(Form):
     """Create form for """
     address: str = TextAreaField('Address', validators=[
         DataRequired('Please enter the address')])
+    full_address = TextAreaField('Full address', render_kw={'readonly': True})
     distance = TextField('Distance', render_kw={'readonly': True})
 
     def validate_address(form, field):
         address: str = field.data
+        # list_toponims = get_toponims(address)
+        # dict_toponims = make_dict(list_toponims)
+        # if 'country' and 'locality' in dict_toponims.keys():
+
+        # if dict_toponims['locality'] != 'Москва':
+        #     pass
+        # elif not dict_toponims['locality']:
+        #     raise ValidationError('')
+        # elif dict_toponims['locality'] == 'Москва':
+        #     raise ValidationError('Уточните адрес, укажите улицу(станцию метро), номер дома')
+        # if len(location.address.split()) == len(address.split()):
+        #     raise ValidationError('Need more information about address')
         if len(address.split()) == 1 and 'Russia' in address:
             raise ValidationError('Add name of locality (city, town etc.)')
         # if len(field.data) <= 1:
@@ -25,3 +42,4 @@ class CalculateDistanceForm(Form):
         except (AttributeError, TypeError):
             raise ValidationError(
                 'The "address" field must contain at least name of the country or name of the locality or both.')
+        return address
