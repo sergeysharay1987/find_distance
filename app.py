ff7471b00078603e07ed7075c96499da6095aead
serg <sergeysharay1987@gmail.com>
2021-12-15 21:41:02 +0300

    Comments and some imports were removed

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-
-import logging
-from logging.config import dictConfig
 from flask import Flask
 from config import Configuration
 from calculated_distance.blueprint import calculated_distance
@@ -9,7 +6,3 @@ application = Flask(__name__)
 application.config.from_object(Configuration)
 application.register_blueprint(calculated_distance, url_prefix='/calculate_distance')
 
-# logger = application.logger
-
-# logging.basicConfig()
-# logging.basicConfig(filename='demo.log', level=logging.DEBUG)
