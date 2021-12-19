396cfb352c35d84101b535ed12e07b19dfd30d84
serg <sergeysharay1987@gmail.com>
2021-12-19 23:22:28 +0300

    Some cnages were made

--- app.py
+++ app.py
@@ -5,4 +5,3 @@ from calculated_distance.blueprint import calculated_distance
 application = Flask(__name__)
 application.config.from_object(Configuration)
 application.register_blueprint(calculated_distance, url_prefix='/calculate_distance')
-
