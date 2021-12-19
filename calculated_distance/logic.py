d859481bcb6cf3243b42943247e5795c46473dda
serg <sergeysharay1987@gmail.com>
2021-12-19 23:11:34 +0300

    Comments including code were removed

--- calculated_distance/logic.py
+++ calculated_distance/logic.py
@@ -7,11 +7,7 @@ from geopy.geocoders import Yandex
 from loguru import logger
 from shapely.geometry import Polygon, Point
 from shapely.ops import nearest_points
-from .blueprint import blueprint_root
-#from app import application
-from app import application
 
-app_root = application.root_path
 
 API_KEY = 'cbddbd2c-95ce-4aa1-ba5a-5d0416597c20'
 ya_geocoder: Yandex = Yandex(api_key=API_KEY)  # геокодер, используемый для геокодирования адреса
@@ -22,7 +18,6 @@ coords_mkad: list = []
 blueprint: str = 'calculated_distance'
 shape_file = 'dataframe.shp'
 list_mkad_s_km: List[float] = []  # список, для хранения координат каждого километра МКАД
-dir_to_file: str = os.getcwd() + '/' + blueprint
 
 
 def make_list_of_addresses_for_request():
@@ -57,29 +52,39 @@ def make_lan_lon_coords(gdf: GeoDataFrame) -> List[Point]:
     return coords_mkad
 
 
+def get_blprt_root():
+    """Возвращает путь до директории "calculated_distance" """
+    from calculated_distance.blueprint import calculated_distance
+    return calculated_distance.root_path
+
+
 def check_file() -> None:
+    calculated_distance_path = get_blprt_root()
     """Проверяет есть ли .shp файл в директории <calculated_distance>"""
-    print(f'dir_to_shape_file: {dir_to_file}')
     # проверяем есть ли файл .shp в текущей директории
-    if shape_file in os.listdir(dir_to_file):
+    # if shape_file in os.listdir(dir_to_file):
+    if shape_file in os.listdir(calculated_distance_path):
         # если .shp файл, есть ничего не делаем
         pass
 
-    elif shape_file not in os.listdir('.') or shape_file in os.listdir('.'):
+    elif shape_file not in os.listdir(calculated_distance_path):
         # создаём .shp файл, если его не было в текущей директории и сохраняем в него координаты всех километров МКАД
         create_geodataframe().to_file(shape_file)
 
 
 def get_polygon(shp_file: str) -> Polygon:
     """Возвращает полигон, содержащий координаты точек каждого километра МКАД"""
-    gdf = geopandas.read_file(dir_to_file + '/' + shp_file)
+    calculated_distance_path = get_blprt_root()
+    # gdf = geopandas.read_file(dir_to_file + '/' + shp_file)
+    gdf = geopandas.read_file(calculated_distance_path + '/' + shp_file)
     coords = make_lan_lon_coords(gdf)
     poly_mkad = Polygon(coords)
     return poly_mkad
 
 
 def write_in_log(address: str, distance: float):
-    path = f'{os.getcwd()}/{blueprint}/info.log'
+    #path = f'{os.getcwd()}/{blueprint}/info.log'
+    path = get_blprt_root()
     logger.add(path, format='{time} {message}', level='INFO')
     logger.info(f'Расстояние от МКАД до {address} равно {distance} км')
 
@@ -89,18 +94,14 @@ def find_distance(coords_of_address) -> Union[int, float]:
     # получаем координаты всех километров МКАД из .xlsx файла
     check_file()
     poly_mkad = get_polygon(shape_file)
-    print(f'find_distance: {poly_mkad.contains(coords_of_address)}')
     if poly_mkad.contains(coords_of_address):
-        print(f'coords_of_address if poly_mkad.contains(coords_of_address):{coords_of_address}')
         return 0
     # находим точку на полигоне, ближайшую к точке, содержащей координаты адреса <coords_of_address>,
     # для которого требуется найти расстояние
     elif not poly_mkad.contains(coords_of_address):
-        print(f'coords_of_address elif not poly_mkad.contains(coords_of_address):{coords_of_address}')
         # ищем точку (nearest_pt) на МКАД,
         # расположенную ближе всего к
         # точке, с координатами адреса, расстояние до которого требуется найти
         p1, nearest_pt = nearest_points(coords_of_address, poly_mkad)
-        print(f'nearest_pt: {nearest_pt}')
         distance = geodesic((nearest_pt.x, nearest_pt.y), (coords_of_address.x, coords_of_address.y))
         return round(distance.km, 1)
