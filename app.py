effb5f7829d7b1b3bde53b7909bc9f38df92af8d
serg <sergeysharay1987@gmail.com>
2021-12-27 17:42:17 +0300

    Create a nwe empty string

--- app.py
+++ app.py
@@ -5,3 +5,4 @@ from mkad_distance.blueprint import mkad_distance
 application = Flask(__name__)
 application.config.from_object(Configuration)
 application.register_blueprint(mkad_distance, url_prefix='/calculate_distance')
+
