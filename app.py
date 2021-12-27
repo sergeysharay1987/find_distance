a9436b2708d019f06d4c44f35e1059d86cc69f82
serg <sergeysharay1987@gmail.com>
2021-12-27 17:41:37 +0300

    Import 'calculated_distance' was replaced by 'mkad_distance'

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from config import Configuration
-from calculated_distance.blueprint import calculated_distance
+from mkad_distance.blueprint import mkad_distance
 
 application = Flask(__name__)
 application.config.from_object(Configuration)
-application.register_blueprint(calculated_distance, url_prefix='/calculate_distance')
+application.register_blueprint(mkad_distance, url_prefix='/calculate_distance')
