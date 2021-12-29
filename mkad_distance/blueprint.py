fbf70c942c6d2f3e5c6a671ffa4488d42d97ba72
serg <sergeysharay1987@gmail.com>
2021-12-29 15:14:34 +0300

    String 'mkad_distance'were replaced by variable 'blprt_name'.

--- mkad_distance/blueprint.py
+++ mkad_distance/blueprint.py
@@ -2,17 +2,17 @@ from flask import Blueprint, render_template
 from geopy import Location
 from mkad_distance.forms import CalculateDistanceForm
 from flask import request
-from mkad_distance.logic import ya_geocoder, find_distance, write_in_log, Point
+from .logic import ya_geocoder, find_distance, write_in_log, Point
 
-name: str = 'mkad_distance'
-mkad_distance: Blueprint = Blueprint(name, __name__, template_folder='templates')
+blprt_name: str = 'mkad_distance'  # название папки blueprint
+mkad_distance: Blueprint = Blueprint(blprt_name, __name__, template_folder='templates')
 
 
 @mkad_distance.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'GET':
         form = CalculateDistanceForm()
-        return render_template('mkad_distance/index.html', form=form)
+        return render_template(f'{blprt_name}/index.html', form=form)
 
     if request.method == 'POST':
         address = request.form['address']
@@ -25,7 +25,7 @@ def index():
             bound_form = CalculateDistanceForm(data={'address': address,
                                                      'full_address': full_address,
                                                      'distance': find_distance(coords_address)})
-            write_in_log(full_address, find_distance(coords_address))
-            return render_template('mkad_distance/index.html', form=bound_form)
+            write_in_log(full_address, bound_form.data['distance'])
+            return render_template(f'{blprt_name}/index.html', form=bound_form)
         elif not bound_form.validate():
-            return render_template('mkad_distance/index.html', form=bound_form)
+            return render_template(f'{blprt_name}/index.html', form=bound_form)
