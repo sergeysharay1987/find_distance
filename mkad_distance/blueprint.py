75d3ea5e3a14f2fe6f82e7bd747e666318d84972
serg <sergeysharay1987@gmail.com>
2022-01-06 23:26:36 +0300

    Some changes were made to solve problem with import's error

--- mkad_distance/blueprint.py
+++ mkad_distance/blueprint.py
@@ -2,30 +2,40 @@ from flask import Blueprint, render_template
 from geopy import Location
 from mkad_distance.forms import CalculateDistanceForm
 from flask import request
-from .logic import ya_geocoder, find_distance, write_in_log, Point
+from shapely.geometry import Point
+from .logic import ya_geocoder, shape_file, check_file, get_polygon, find_distance, write_in_log
 
 blprt_name: str = 'mkad_distance'  # название blueprint'а и название папки расположения blueprint'а
 mkad_distance: Blueprint = Blueprint(blprt_name, __name__, template_folder='templates')
+blpt_root = mkad_distance.root_path
+data_dir = f'{blpt_root}/data'
+poly_mkad = get_polygon(shape_file, data_dir)
 
 
 @mkad_distance.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'GET':
+
         form = CalculateDistanceForm()
         return render_template(f'{blprt_name}/index.html', form=form)
 
     if request.method == 'POST':
-        address = request.form['address']
-        bound_form = CalculateDistanceForm(data={'address': address})
+        data = request.form.copy()
+        bound_form = CalculateDistanceForm(data=data)
         if bound_form.validate():
+
+            check_file(data_dir)
             address = bound_form.data['address']
             loc_address: Location = ya_geocoder.geocode(address)
             coords_address: Point = Point(loc_address._tuple[1])
             full_address = loc_address.address
+            distance = find_distance(coords_address, poly_mkad)
             bound_form = CalculateDistanceForm(data={'address': address,
                                                      'full_address': full_address,
-                                                     'distance': find_distance(coords_address)})
-            write_in_log(full_address, bound_form.data['distance'])
+                                                     'distance': f'{distance} км'})
+            write_in_log(full_address, distance, blpt_root)
             return render_template(f'{blprt_name}/index.html', form=bound_form)
         elif not bound_form.validate():
+            bound_form.full_address.data = ''
+            bound_form.distance.data = ''
             return render_template(f'{blprt_name}/index.html', form=bound_form)
